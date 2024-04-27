7261d14c763cf84f1e3c0f33f83612e79796aef9
ニューエスト <epiccatface000@gmail.com>
2024-04-27 15:37:25 +0800

    Update Roblox-WebP-To-PNG.js to v1.2
    Update Roblox-WebP-To-PNG.js to v1.2

---
+++ Roblox-WebP-To-PNG.js
@@ -0,0 +1,83 @@
+// This code changes all the lossy versions of the images on Roblox to be lossless.
+
+// Function to replace image source:
+function replaceImageSource(element, partialOriginalSrc, newSrc) {
+    var image = element.querySelector('img');
+    if(image) {
+        var currentSrc = image.getAttribute('src');
+        if(currentSrc.includes(partialOriginalSrc)) {
+            var newSrcWithReplacement = currentSrc.replace(partialOriginalSrc, newSrc);
+            image.setAttribute('src', newSrcWithReplacement);
+        }
+    }
+}
+
+// Watch for changes in the DOM:
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        if (mutation.type === 'childList') {
+            mutation.addedNodes.forEach(function(node) {
+                if (node.nodeType === Node.ELEMENT_NODE) {
+                    replaceImageSources(node);
+                }
+            });
+        }
+    });
+});
+var observerConfig = { childList: true, subtree: true };
+observer.observe(document, observerConfig);
+
+// Queue to store images waiting for replacement:
+var replacementQueue = [];
+var isProcessingQueue = false;
+
+// Function to replace image sources for specified element:
+function replaceImageSources(element) {
+    var ProfileBodyShot = element.querySelectorAll('.thumbnail-2d-container.thumbnail-span');
+    var ProfileHeadShot = element.querySelectorAll('.thumbnail-2d-container.avatar-card-image.profile-avatar-thumb');
+    var HomeFriendList = element.querySelectorAll('.thumbnail-2d-container');
+    var GameIcons1 = element.querySelectorAll('.thumbnail-2d-container.game-card-thumb-container');
+    var GameIcons2 = element.querySelectorAll('.thumbnail-2d-container.brief-game-icon');
+    var GameIconsDiscover = element.querySelectorAll('.thumbnail-2d-container.game-card-thumb');
+    var MarketplaceIcons = element.querySelectorAll('.ng-isolate-scope');
+    var ItemPage= element.querySelectorAll('.thumbnail-2d-container');
+
+    // Replace image sources for the first set of elements
+    enqueueReplacements(ProfileBodyShot, 'Webp/noFilter', 'PNG/noFilter');
+    enqueueReplacements(ProfileHeadShot, 'Webp/noFilter', 'PNG/noFilter');
+    enqueueReplacements(HomeFriendList, 'Webp/noFilter', 'PNG/noFilter');
+    enqueueReplacements(GameIcons1, 'Webp', 'PNG');
+    enqueueReplacements(GameIcons2, 'Webp/noFilter', 'PNG/noFilter');
+    enqueueReplacements(GameIconsDiscover, 'Webp', 'PNG');
+    enqueueReplacements(MarketplaceIcons, 'Webp', 'PNG');
+    enqueueReplacements(ItemPage, 'Webp', 'PNG');
+
+    processQueue();
+}
+
+// Function to enqueue replacements:
+function enqueueReplacements(elements, partialOriginalSrc, newSrc) {
+    elements.forEach(function(element) {
+        replacementQueue.push({ element: element, partialOriginalSrc: partialOriginalSrc, newSrc: newSrc });
+    });
+}
+
+// Function to process replacement queue:
+function processQueue() {
+    if (!isProcessingQueue && replacementQueue.length > 0) {
+        isProcessingQueue = true;
+        var replacement = replacementQueue.shift();
+        replaceImageSource(replacement.element, replacement.partialOriginalSrc, replacement.newSrc);
+        setTimeout(function() {
+            isProcessingQueue = false;
+            processQueue();
+        }, 0);
+    }
+}
+
+// Call the function once initially to replace image sources on page load:
+replaceImageSources(document);
+
+// v1.2 
+// https://github.com/Knewest
+// Copyright (Boost Software License 1.0) 2024-2024 Knew
